0c8d7edcdc8b2b717a6d35407a22414a486c5255
cs0501 <960868236@qq.com>
2021-09-19 19:36:45 +0800

    Update work.cpp
    能够不识别注释和文本中的关键字

--- work.cpp
+++ work.cpp
@@ -13,10 +13,14 @@ string stand[]={"auto","break","case","char","const","continue","default","do",
     "int","long","register","return","short","signed","sizeof","static",
     "struct","switch","typedef","union","unsigned","void","volatile","while"};
 
+int pairing(string target); //配对成功则返回该关键字的号码，配对失败，则返回-1；
+
 int main(){
+    int total_num = 0;
+    int senten_jd = 0;      //判断是否处于语句中，为奇数时表示在语句中，不需要识别关键字
+    int annot_jd = 0;       //用于定义是否处于长注释中
     char buf[MAX_LINE];  /*缓冲区*/
     FILE *fp;            /*文件指针*/
-    int len;             /*行字符个数*/
     int len,n,li;             /*所读到的字符下标,li记录单词的长度*/
     string word_ = "abcdefgh";  //代码中读取进的格式为char，设置word_ 用于转存
     string word;        //用于存储所要进行判断的字符串
@@ -34,20 +38,24 @@ int main(){
                 word_[li] = buf[i];
                 li++;
             }
+            else if(buf[i] == '/'&&buf[i+1] == '/') break;  //当一行中出现注释时，直接跳过这一行
+            else if(buf[i] == '"') senten_jd++;           //当出现引号时，改变判断状态
+            else if(buf[i] == '/'&&buf[i+1] == '*'&&annot_jd == 0) annot_jd = 1;  //当annot_jd为1时进入长注释
+            else if(buf[i] == '*'&&buf[i+1] == '/'&&annot_jd == 1) annot_jd = 0;  //当annot_jd为0时离开长注释
             else{
                 word = word_.substr(0,li);
-                if(pairing(word) >= 0){    
+                if(pairing(word) >= 0&&senten_jd%2 == 0&&annot_jd == 0){    
                     cout<<word<<endl;
                     total_num++;    
                 }
                 li = 0;
             }
         }
-        word = word_.substr(0,li);
+        /*word = word_.substr(0,li);
         if(pairing(word)>=0){    
             cout<<word<<endl;
             total_num++;    
-        }
+        }*/
     }
     cout<<total_num<<endl;
     system("pause");
