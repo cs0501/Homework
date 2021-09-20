23e3dc510278fb10c58f87dde985af8261868ad4
cs0501 <960868236@qq.com>
2021-09-20 23:19:03 +0800

    Create test0.cpp
    测试文件

---
+++ test0.cpp
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<string>
+using namespace std;
+
+int main() {
+	int i = 0;
+	int iswitch = 0;
+	int jcase = 0;
+	
+	switch(iswitch){		//ooo switch case ooo
+        case 0:
+            break;
+        case 1:
+            if(iswitch && jcase){
+            	;
+			}
+			else if(iswitch){
+			;}
+			else if(jcase){}
+			if(iswitch == 10){;}
+			else{}
+        case 2:
+            printf("oh case oh");
+        default:
+            break;
+    }
+    switch(jcase){       //ooo case case ooo
+        case 0:
+            break;
+        case 1:
+            break;
+        default:
+            printf("oh switch oh");
+    }
+
+
+	if(i<0) {
+		if(i<-1) {}
+		else printf("oh if else oh");
+	} else if(i>0) {
+		if (i>2) {}
+		else if (i==2) {}
+		else if (i>1) {}
+		else { 
+			if(i==0) {}
+			else {}
+		};
+	}
+	iswitch = jcase;  /*
+	ohh switch case */
+	/*switch*/if(i > 8){  /*oh case*/
+		if (i <7){
+			i=9;
+		}
+		else if(i <4) {	string a = "oh if else oh", b = "oh if else";}
+		else i=10;
+	}
+	/*
+	oh if else oh
+	oh if else oh
+	*/
+	/*case*/ else{	/*oh if else oh*/
+		;
+	}
+	/*if**/
+	
+	for(int i = 0; i < 50; i++){
+		printf("%d\n", i);
+		if((i + 1) % 10 == 0){
+			i++;
+		}		
+	}
+	
+	
+	return 0;
+}
+
