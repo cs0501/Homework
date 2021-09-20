30c89956dc7f4a06b4f60769bad2477eafe1b23d
cs0501 <960868236@qq.com>
2021-09-20 14:17:08 +0800

    Update work.cpp
    增加了switch和case的计数和输出

--- work.cpp
+++ work.cpp
@@ -13,16 +13,23 @@ string stand[]={"auto","break","case","char","const","continue","default","do",
     "int","long","register","return","short","signed","sizeof","static",
     "struct","switch","typedef","union","unsigned","void","volatile","while"};
 
+int case_nums[10000];      //用于存储每个switch后case的数量
+
 int pairing(string target); //配对成功则返回该关键字的号码，配对失败，则返回-1；
 
 int main(){
     int total_num = 0;
+    int switch_num = -1;
+    int case_num = 0;
+    int pairs;               //用于存储pairing函数的返回值
+    int senten_jd = 0;      //判断是否处于语句中，为奇数时表示在语句中，不需要识别关键字
+    int annot_jd = 0;       //用于定义是否处于长注释中
     char buf[MAX_LINE];  /*缓冲区*/
     FILE *fp;            /*文件指针*/
     int len,n,li;             /*所读到的字符下标,li记录单词的长度*/
     string word_ = "abcdefgh";  //代码中读取进的格式为char，设置word_ 用于转存
     string word;        //用于存储所要进行判断的字符串
-    if((fp = fopen("D:\\VSCode\\test.txt","r")) == NULL){
+    if((fp = fopen("D:\\VSCode\\test1.txt","r")) == NULL){
         perror("fail to read");
         exit (1) ;
     }
@@ -36,23 +43,50 @@ int main(){
                 word_[li] = buf[i];
                 li++;
             }
+            else if(buf[i] == '/' && buf[i+1] == '/') break;  //当一行中出现注释时，直接跳过这一行
+            else if(buf[i] == '"') {senten_jd++; li =0;}             //当出现引号时，改变判断状态
+            else if(buf[i] == '/' && buf[i+1] == '*' && annot_jd == 0) {annot_jd = 1; li = 0;}    //当annot_jd为1时进入长注释
+            else if(buf[i] == '*' && buf[i+1] == '/' && annot_jd == 1) {annot_jd = 0; li = 0;}    //当annot_jd为0时离开长注释
             else{
                 word = word_.substr(0,li);
-                if(pairing(word) >= 0){    
+                pairs = pairing(word);
+                if(pairs >= 0 && senten_jd%2 == 0 && annot_jd == 0){    
+                    if(pairs == 25){         //对每个switch后的case进行计数和存储
+                        if(switch_num == -1) switch_num++;
+                        else{
+                            case_nums[switch_num++] = case_num;
+                            case_num = 0;
+                        }    
+                    }
+                    else if(pairs == 2) case_num++;             //遇到case时计数
                     cout<<word<<endl;
                     total_num++;    
                 }
                 li = 0;
             }
         }
-        word = word_.substr(0,li);
-        if(pairing(word)>=0){    
-            cout<<word<<endl;
-            total_num++;    
-        }
     }
-    cout<<total_num<<endl;
+    if(case_num > 0) case_nums[switch_num++] = case_num;
+    cout<<"total num: "<<total_num<<endl;
+    cout<<"switch num: "<<switch_num<<endl;
+    cout<<"case num: ";
+    for(int i = 0; i < switch_num; i++) cout<<case_nums[i]<<" ";
+    cout<<endl;
     system("pause");
     return 0;
 }
 
+int pairing(string target){
+    int head = 0,tail = 31,k;
+    while(head <= tail){
+        k = (head + tail)/2;
+        if(target.compare(stand[k]) < 0){
+            tail = k-1;
+        }
+        else if(target.compare(stand[k]) > 0){
+            head = k+1;
+        }
+        else return k;
+    }
+    return -1;
+}
