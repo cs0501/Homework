86e1cc33d5435441ca27c8959ba63b82aced8093
cs0501 <960868236@qq.com>
2021-09-20 18:27:35 +0800

    Update work.cpp
    初步实现if-else的计数，但存在bug，只有部分样例可以运行

--- work.cpp
+++ work.cpp
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include<iostream>
-#include<bits/stdc++.h>
+#include <stack>
+#include <iostream>
+#include <bits/stdc++.h>
 #define MAX_LINE 1024
 
 using namespace std;
 
-string stand[]={"auto","break","case","char","const","continue","default","do",
+string stand[] = {"auto","break","case","char","const","continue","default","do",
     "double","else","enum","extern","float","for","goto","if",
     "int","long","register","return","short","signed","sizeof","static",
     "struct","switch","typedef","union","unsigned","void","volatile","while"};
@@ -21,7 +22,11 @@ int main(){
     int total_num = 0;
     int switch_num = -1;
     int case_num = 0;
-    int pairs;               //用于存储pairing函数的返回值
+    int else_num = 0;       //else_num用于存储if-else结构和if-else if-else结构的总数量
+    int if_else = 0;        //用于存储if-else结构的数量
+    int pairs;              //用于存储pairing函数的返回值
+    stack<string> if_jd;    //判断if-else结构是否计数
+    if_jd.push("if");
     int senten_jd = 0;      //判断是否处于语句中，为奇数时表示在语句中，不需要识别关键字
     int annot_jd = 0;       //用于定义是否处于长注释中
     char buf[MAX_LINE];  /*缓冲区*/
@@ -44,24 +49,53 @@ int main(){
                 li++;
             }
             else if(buf[i] == '/' && buf[i+1] == '/') break;  //当一行中出现注释时，直接跳过这一行
-            else if(buf[i] == '"') {senten_jd++; li =0;}             //当出现引号时，改变判断状态
-            else if(buf[i] == '/' && buf[i+1] == '*' && annot_jd == 0) {annot_jd = 1; li = 0;}    //当annot_jd为1时进入长注释
-            else if(buf[i] == '*' && buf[i+1] == '/' && annot_jd == 1) {annot_jd = 0; li = 0;}    //当annot_jd为0时离开长注释
+            else if(buf[i] == '"') {            //当出现引号时，改变判断状态
+                senten_jd++; 
+                li =0;
+            }                              
+            else if(buf[i] == '/' && buf[i+1] == '*' && annot_jd == 0) {    //当annot_jd为 1 时进入长注释
+                annot_jd = 1; 
+                li = 0;
+            }    
+            else if(buf[i] == '*' && buf[i+1] == '/' && annot_jd == 1) {    //当annot_jd为 0 时离开长注释
+                annot_jd = 0; 
+                li = 0;
+            } 
+            else if(buf[i] == '}'){
+                while (if_jd.top() != "{") {cout<<if_jd.top()<<endl; if_jd.pop();}
+                cout<<"OUT  "<<if_jd.top()<<endl;
+                if_jd.pop();    
+            } 
             else{
+                if(buf[i] == '{') if_jd.push("{");
                 word = word_.substr(0,li);
                 pairs = pairing(word);
                 if(pairs >= 0 && senten_jd%2 == 0 && annot_jd == 0){    
-                    if(pairs == 25){         //对每个switch后的case进行计数和存储
+                    if(pairs == 25){         //对每个 switch 后的 case 进行计数和存储
                         if(switch_num == -1) switch_num++;
                         else{
                             case_nums[switch_num++] = case_num;
                             case_num = 0;
                         }    
                     }
-                    else if(pairs == 2) case_num++;             //遇到case时计数
+                    else if(pairs == 2) case_num++;             //遇到 case 时计数
+                    else if(pairs == 15) if_jd.push("if");      //遇到 if 时将 if 入栈
+                    else if(pairs == 9){
+                        if(!(buf[i] == ' ' && buf[i + 1] == 'i' && buf[i + 2] == 'f')){
+                            else_num++;
+                            cout<<"else_num  "<<else_num<<endl;
+                            if(if_jd.top() == "if"){ 
+                                if_else++;
+                                cout<<"if_else  "<<if_else<<endl;
+                                if_jd.pop();
+                            } 
+                        }
+                        else if(if_jd.top() == "if") if_jd.pop();
+                    }
                     cout<<word<<endl;
                     total_num++;    
                 }
+                
                 li = 0;
             }
         }
@@ -72,6 +106,8 @@ int main(){
     cout<<"case num: ";
     for(int i = 0; i < switch_num; i++) cout<<case_nums[i]<<" ";
     cout<<endl;
+    cout<<"if-else num: "<<if_else<<endl;
+    cout<<"if-elseif-else num: "<<else_num - if_else<<endl;
     system("pause");
     return 0;
 }
